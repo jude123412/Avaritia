ad477d2825478f2c18db4e72ac045807c79d0cf0
jude123412 <104434396+jude123412@users.noreply.github.com>
2024-12-23 21:40:28 +0000

    Update ShaderHelper.java
    Increase Cosmic Shader Duration to 24000 ticks instead of 12000

--- src/main/java/morph/avaritia/client/render/shader/ShaderHelper.java
+++ src/main/java/morph/avaritia/client/render/shader/ShaderHelper.java
@@ -48,10 +48,10 @@ public final class ShaderHelper {
         if (shader != 0) {
             Minecraft mc = Minecraft.getMinecraft();
             int time = ARBShaderObjects.glGetUniformLocationARB(shader, "time");
-            int animationTime = (int) (mc.player.ticksExisted % 12000);
+            int animationTime = (int) (mc.player.ticksExisted % 24000);
 
             if (mc.player != null && mc.player.world != null) {
-                ARBShaderObjects.glUniform1iARB(time, animationTime <= 6000 ? animationTime : animationTime * -1 + 12000);
+                ARBShaderObjects.glUniform1iARB(time, animationTime <= 12000 ? animationTime : animationTime * -1 + 24000);
             }
 
             if (callback != null) {
